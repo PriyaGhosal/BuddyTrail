80c91af646a58cc7fc6b604755fac308c59e78ca
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-10 12:09:53 +0530

    Nav Bar Shadow added

--- script.js
+++ script.js
@@ -316,6 +316,15 @@
     
 // }
 
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("main-head");
+    if (window.scrollY > 100) {
+      navbar.classList.add("shadow");
+    } else {
+      navbar.classList.remove("shadow");
+    }
+});
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
