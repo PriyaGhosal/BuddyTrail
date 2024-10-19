d01ab89a7d63973a897d16e3afa66792156cf1c3
Aditya Bakshi <153073510+Aditya90456@users.noreply.github.com>
2024-10-19 12:50:39 +0530

    authentication is complete

--- auth.js
+++ auth.js
@@ -3,7 +3,47 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const gbutton=document.getElementById("auth");
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { GoogleAuthProvider ,getAuth,signInWithPopup} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
+const provider = new GoogleAuthProvider();  
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcEfpZEVUcYBYZmEMl1B5MJ6Q8Da8YQro",
+  authDomain: "buddytail.firebaseapp.com",
+  projectId: "buddytail",
+  storageBucket: "buddytail.appspot.com",
+  messagingSenderId: "37897826629",
+  appId: "1:37897826629:web:6c396436d57d987d42ce2c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+gbutton.addEventListener("click",function(){
+
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+
+ 
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used. 
+    // ...
+  });
+})
 // Allowed email domains for registration
 const allowedDomains = ['gmail.com', 'outlook.com', 'yahoo.com', 'hotmail.com' , 'icloud.com' , 'protonmail.com' , 'tutanota.com']; // Add more as needed
 
