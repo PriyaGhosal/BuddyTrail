0d1f77bc843c9c55bbb1a7763ebf3bdf4506bb8f
haseebzaki-07 <haseebzaki13@gmail.com>
2024-10-27 15:51:25 +0530

    Add ratings

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');  // Database connection
 const authRoutes = require('./routes/authRoutes');  // Authentication routes
 const ContactRoutes = require('./routes/ContactRoutes');  
+const RatingRoutes = require('./routes/RatingRoutes');  
 const cookieParser = require('cookie-parser');  // Middleware for parsing cookies
 const config = require('./config/config');  // Config file for environment variables
 const cors = require('cors');  // Middleware for Cross-Origin Resource Sharing
@@ -26,6 +27,7 @@ app.use(cookieParser());  // Enable cookie parsing
 // API routes
 app.use('/api/auth', authRoutes); 
 app.use('/api/contact', ContactRoutes); 
+app.use('/api/rating', RatingRoutes); 
 
 // Server listening on configured port
 const PORT = config.port || 5000;  // Use config for port, default to 5000 if undefined
