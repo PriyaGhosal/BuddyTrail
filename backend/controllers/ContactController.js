5d804c07fa14cb3859faa0dc572cdb5b932cab02
Ayush Sharma <asharma7588@gmail.com>
2024-10-28 16:50:43 +0530

    Update ContactController.js

--- backend/controllers/ContactController.js
+++ backend/controllers/ContactController.js
@@ -19,8 +19,8 @@ exports.sendEmail = async (req, resp) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "taskmaster991@gmail.com",
-      pass: "kmepakzcabvztekd",
+      user: "your email id",
+      pass: "your password",
     },
   });
 
